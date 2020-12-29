8065f0ec4909a6fbed72cec56b21282d0fc66ec7
xokc <xokc@mail.ru>
2020-12-29 16:58:53 +0300

    Extract rendering in separate file Add timestamp rendering in log Set coloring scheme

---
+++ dmd-render.hpp
@@ -0,0 +1,161 @@
+#pragma once
+
+#include <cmath>
+#include <string>
+#include <chrono>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+#include <ftxui/dom/elements.hpp>
+
+namespace dmd {
+
+struct State {
+    static std::wstring to_ws(std::chrono::milliseconds ms) {
+        using namespace std::chrono;
+        seconds s = duration_cast<seconds>(ms);
+        minutes m = duration_cast<minutes>(ms);
+        hours   h = duration_cast<hours>(ms);
+        wchar_t buf[6];
+        swprintf(buf, sizeof(buf), L"%.2d:%.2d", m.count() % 60, s.count() % 60);
+        std::wstring ts{buf};
+        if (h.count() > 0) ts.insert(0, std::to_wstring(h.count()) + L':');
+        return ts;
+    };
+
+    struct {
+        std::wstring title;
+        std::wstring value;
+    } header;
+
+    struct Log {
+        std::vector<std::pair<std::wstring, std::wstring>> buffer;
+
+        void push(std::chrono::milliseconds cur_ms, const std::wstring &line) {
+            auto timestamp = to_ws(cur_ms) + L' ';
+            if (buffer.size() > 100) buffer.erase(buffer.begin());
+            buffer.emplace_back(timestamp, line);
+        }
+    } log;
+
+    struct Progress {
+        std::wstring prefix;
+        unsigned position;
+        unsigned limit;
+        std::wstring suffix;
+
+        Progress() : position{0}, limit{0} {}
+
+        [[nodiscard]] auto value() const { return (limit) ? std::min(100.0f, float(position) / float(limit)) : 0; }
+
+        [[nodiscard]] std::wstring percent() const {
+            wchar_t buf[6];
+            swprintf(buf, sizeof(buf), L"%3d%% ", std::lround(value() * 100));
+            return buf;
+        }
+
+        void update(std::chrono::milliseconds cur_ms, unsigned shift) {
+            using namespace std::chrono;
+            position += shift;
+            milliseconds eta_ms{std::lround(cur_ms.count() / value())};
+            if (cur_ms > eta_ms) eta_ms = cur_ms;
+            std::wstringstream ss;
+            ss << std::fixed << std::setprecision(1) << position * 1000.0 / cur_ms.count();
+
+            suffix = L" " + to_ws(cur_ms) + L"<" + to_ws(eta_ms) + L", " + ss.str() + L"it/s ";
+        }
+    } progress;
+
+    struct Status {
+        std::wstring title;
+        unsigned value;
+
+        Status() : value{0} {}
+    } status;
+
+    std::chrono::steady_clock::time_point start;
+
+    State() : start{std::chrono::steady_clock::now()} {}
+
+    void push_progress(unsigned shift, const std::wstring &log_line) {
+        using namespace std::chrono;
+        auto cur_ms = duration_cast<milliseconds>(steady_clock::now() - start);
+        log.push(cur_ms, log_line);
+        progress.update(cur_ms, shift);
+    }
+};
+
+static auto makeDoc(const State &state) {
+    using namespace ftxui;
+    static const unsigned pw = 20, ph = 9;
+    Elements log_lines;
+    const auto &lines = state.log.buffer;
+    for (auto it = lines.cend() - std::min<unsigned>(lines.size(), ph - 3); it < lines.end(); ++it) {
+        auto line = *it;
+//TODO Add tripoints for long lines
+//        for (int x = 0; x < std::min<unsigned>(line.size(), maxWidth); ++x)
+//            image(x, y).c = line[x];
+//        if (line.size() > maxWidth) {
+//            image(maxWidth - 1, y).c = '…';
+//        }
+        log_lines.push_back(hbox({
+            color(Color::GrayDark, text(line.first)),
+            color(Color::Cyan, text(line.second))
+        }));
+    }
+
+    return
+        vbox({
+            hbox({
+                vbox({
+                    hbox({text(state.header.title), color(Color::DarkGreen, text(state.header.value))}),
+                    hbox({
+                        text(state.progress.prefix),
+                        color(Color::CyanLight, text(state.progress.percent())),
+                        color(Color::CyanLight, gauge(state.progress.value())) | flex,
+                        color(Color::GrayDark, text(state.progress.suffix)),
+                    }),
+                    vbox(log_lines) | size(HEIGHT, EQUAL, ph - 3),
+                    text(state.status.title),
+                }) | flex,
+                bgcolor(Color::GrayDark, text(L"+") | hcenter | vcenter)
+                    | size(WIDTH, EQUAL, 20) | size(HEIGHT, EQUAL, ph)
+            }),
+        });
+}
+
+class StateRender {
+public:
+    StateRender() = default;
+    virtual ~StateRender() = default;
+    virtual void render(const State &state) const = 0;
+};
+
+class TuiRender : public StateRender {
+public:
+    TuiRender() : StateRender() {
+    }
+
+    ~TuiRender() override {
+        std::cout << "\x1B[?25h" << std::flush;
+    }
+
+    void render(const State &state) const override {
+        using namespace ftxui;
+        static Dimension d{-1, -1};
+        if (d.dimx == -1) {
+            d = Dimension::Full();
+            d.dimy = 9 + 1;
+        }
+        if (d.dimx < 1) d.dimx = 80;
+        auto screen = Screen::Create(d);
+        Render(screen, dmd::makeDoc(state));
+        static std::string reset_position;
+        if (reset_position.empty()) std::cout << "\x1B[?25l" << std::flush;
+        std::cout << reset_position << screen.ToString() << std::flush;
+        reset_position = screen.ResetPosition();
+    }
+};
+
+}
---
+++ loren.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+
+std::wstring nextLoren() {
+    static const wchar_t *lorens[] = {
+        L"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        L"Nunc at metus et dui fringilla elementum.",
+        L"Sed pretium sem nec tellus congue porta.",
+        L"Aenean sit amet tellus ultricies, consectetur nunc eu, aliquam lectus.",
+        L"Praesent euismod urna pharetra sapien porttitor, in malesuada diam ornare.",
+        L"Duis eu erat pellentesque, ornare mauris eu, eleifend risus.",
+        L"",
+        L"Proin nec metus non elit tempor sodales.",
+        L"In vel metus aliquet urna aliquam vestibulum.",
+        L"Aliquam vitae odio tristique, vehicula nisl et, tempus ex.",
+        L"Praesent ornare neque in scelerisque faucibus.",
+        L"",
+        L"Proin sagittis risus eu odio lacinia, id vestibulum est ultricies.",
+        L"Proin vel urna consectetur, rhoncus felis a, sodales felis.",
+        L"Quisque feugiat libero et libero ultricies malesuada.",
+        L"",
+        L"Phasellus laoreet sapien in maximus dignissim.",
+        L"Praesent nec odio vehicula, laoreet massa ac, mollis erat.",
+        L"",
+        L"Vivamus eu neque elementum, sagittis nunc sed, volutpat mi.",
+        L"Integer aliquet neque eu vehicula rhoncus.",
+        L"Sed in justo sit amet turpis euismod condimentum.",
+        L"Pellentesque in ex sit amet lacus laoreet ultricies.",
+        L"Ut imperdiet metus eget neque laoreet mattis.",
+        L"Donec consectetur nulla eu volutpat facilisis."
+    };
+    static size_t idx = 0;
+    constexpr size_t lcount = sizeof(lorens) / sizeof(*lorens);
+    return lorens[idx++ % (lcount)];
+}
+
+
